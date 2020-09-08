4c8eca5c1dbd6e47c477af273408f7ec20ef012b
Emmanuel Cobian <emmanuel.cobian@softwareallies.com>
2020-09-07 20:57:11 -0500

    Initial commit

---
+++ src/main/java/com/resume/api/ResuMe/ResuMeApplication.java
@@ -0,0 +1,13 @@
+package com.resume.api.ResuMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResuMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume/api/ResuMe/ResuMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.api.ResuMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResuMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
